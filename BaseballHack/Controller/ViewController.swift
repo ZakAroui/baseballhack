361c44d9e1999eadfb3e37359141fdd88575904c
zacharyesm <zackesmaelzada@gmail.com>
2019-03-09 13:45:36 -0500

    tableView init

---
+++ BaseballHack/Controller/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  BaseballHack
+//
+//  Created by Zack Esm on 3/9/19.
+//  Copyright © 2019 GameChanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .plain)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    private var teams = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        setupViews()
+    }
+    
+    fileprivate func setupViews() {
+        let margins = view.layoutMarginsGuide
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: margins.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: margins.bottomAnchor)
+            ])
+        
+    }
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+}
+

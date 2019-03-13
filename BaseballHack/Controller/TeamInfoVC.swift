46b86ad7bb4e5fa6eedb16bb9c8a6b0e800a5373
Zak <zaki.aroui@gmail.com>
2019-03-12 22:25:06 -0400

    added team info Views

---
+++ BaseballHack/Controller/TeamInfoVC.swift
@@ -0,0 +1,21 @@
+//
+//  TeamInfoVC.swift
+//  BaseballHack
+//
+//  Created by Zak Aroui on 3/9/19.
+//  Copyright © 2019 GameChanger. All rights reserved.
+//
+
+import UIKit
+
+class TeamInfoVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+
+}
--- BaseballHack/Controller/TeamsInfoTableVC.swift
+++ BaseballHack/Controller/TeamsInfoTableVC.swift
@@ -1,3 +1,11 @@
+//
+//  TeamsInfoTableVC.swift
+//  BaseballHack
+//
+//  Created by Zak Aroui on 3/9/19.
+//  Copyright © 2019 GameChanger. All rights reserved.
+//
+
 import UIKit
 
 class TeamsInfoTableViewCell: UITableViewCell {
